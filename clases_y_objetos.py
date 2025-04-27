b13d840b660feec86ee3787bdacb1a7b37d449c1
ImTommyDev <tommy.devlpr@gmail.com>
2025-04-27 14:37:43 +0200

    Usando metodos

--- clases_y_objetos.py
+++ clases_y_objetos.py
@@ -6,9 +6,15 @@ class Telefono():
         self.marca = marca #es como si digo Telefono.marca y lo iguala a la marca que le pasamos
         self.modelo = modelo
         self.color = color
+        
+    #Definición de un método de la clase Telefono
+    def encender(self):
+        print(f"Encendiendo el teléfono {self.marca} {self.modelo}...")
           
     
     
 #Instancio un objeto de la clase Telefono
-telefono1 = Telefono("Samsung", "Galaxy S21","negro")
+telefono1 = Telefono("Samsung", "Galaxy S21","negro") #telefono1 es una instancia de la clase Telefono (es un objeto)
 print(telefono1.marca) #Accedo al atributo marca del objeto telefono1
+
+telefono1.encender() #Llamo al método encender del objeto telefono1
