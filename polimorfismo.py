695bd480faf64a181dd495dd36a4cc4338508d2f
ImTommyDev <tommy.devlpr@gmail.com>
2025-05-02 18:43:42 +0200

    Fin clase de polimorfismo

--- polimorfismo.py
+++ polimorfismo.py
@@ -21,3 +21,17 @@ def hacerSonido(animal):
     
 hacerSonido(gato) 
 hacerSonido(perro) #Esto es polimorfismo, ya que la función hacerSonido() puede recibir diferentes tipos de objetos y el método sonido() se comporta de diferente manera dependiendo del objeto que se le pase como argumento. 
+
+print("-"*20)
+
+#Ejemplo de polimorfismo
+def recorrer(lista):
+    for elemento in lista:
+        print("El elemento es: ", elemento)
+        
+lista = [1, 2, 3, 4, 5]
+palabra = "Buenas"
+
+recorrer(lista)
+
+recorrer(palabra)
